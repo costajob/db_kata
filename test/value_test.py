7c47f226e2cdf502443524a92cd73b3b895e3e7c
costajob <costajob@gmail.com>
2018-09-11 17:13:36 +0200

    Mike - minor corner cases tests

--- test/value_test.py
+++ test/value_test.py
@@ -7,12 +7,20 @@ class TestDatastore(unittest.TestCase):
         ival = value.IntVal(42, _max=42)
         self.assertEqual(ival.val, 42)
 
+    def test_valid_int_by_txt(self):
+        ival = value.IntVal('42')
+        self.assertEqual(ival.val, 42)
+
     def test_invalid_int(self):
         with self.assertRaises(value.Val.RangeError):
             value.IntVal(42, 0, 10)
 
     def test_valid_float(self):
         fval = value.FloatVal(3.14, _min=3.14)
+        self.assertEqual(fval.val, 3.14)
+
+    def test_valid_float_by_str(self):
+        fval = value.FloatVal('3.14')
         self.assertEqual(fval.val, 3.14)
 
     def test_invalid_float(self):
@@ -23,6 +31,10 @@ class TestDatastore(unittest.TestCase):
         tval = value.TxtVal('here come the sun', _max=64)
         self.assertEqual(tval.val, 'here come the sun')
 
+    def test_valid_txt_by_int(self):
+        tval = value.TxtVal(9999)
+        self.assertEqual(tval.val, '9999')
+
     def test_invalid_txt(self):
         with self.assertRaises(value.Val.RangeError):
             value.TxtVal('here come the sun', _min=100)
@@ -31,7 +43,11 @@ class TestDatastore(unittest.TestCase):
         dval = value.DateVal('2017-09-11')
         self.assertEqual(dval.val, date(2017, 9, 11))
 
-    def test_invalid_date(self):
+    def test_invalid_date_value(self):
+        with self.assertRaises(value.Val.RangeError):
+            value.DateVal('2017-09-31')
+
+    def test_invalid_date_format(self):
         with self.assertRaises(value.Val.RangeError):
             value.DateVal('11th of Spetember')
 
