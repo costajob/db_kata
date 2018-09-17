93a8f11a94cf36353b3897ec93ad4efb1632fe50
costajob <costajob@gmail.com>
2018-09-17 14:47:23 +0200

    Mike - updated log file

--- weta_db/logger.py
+++ weta_db/logger.py
@@ -7,7 +7,7 @@ The logger singleton for the main program, tracing to ERROR level by default.
 import logging
 from os import path
 
-FILENAME = path.abspath('./weta_python.log')
+FILENAME = path.abspath('./weta_db.log')
 FORMAT = '[%(asctime)s #%(process)d] -- %(levelname)s : %(message)s'
 logging.basicConfig(filename=FILENAME, format=FORMAT, level=logging.ERROR)
 BASE = logging.getLogger(__name__)
